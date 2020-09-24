c79a7f290f156272c7421aded0997804c0c52e05
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-09-24 00:39:31 +0000

    pair program word search complete

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    let verticalJoin = [];
+    
+    const colSize = letters.length;
+    for (let i = 0; i < colSize; i++) {
+        let wordArr = [];
+        for (const array of letters) {
+            wordArr.push(array[i]);
+        }
+        verticalJoin.push(wordArr);
+    }
+    const verticalJoint = verticalJoin.map(ls => ls.join(''))
+
+    for (l of verticalJoint) {
+        if (l.includes(word)) 
+        return true
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
+
